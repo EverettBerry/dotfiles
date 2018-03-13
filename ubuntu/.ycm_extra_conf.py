2152885f4d2641d348e6b6bf54eb1f6e2d32a141
Everett Berry <everettpberry@gmail.com>
2018-03-13 15:12:33 -0400

    forgot some libraries, fix indenting

--- ubuntu/.ycm_extra_conf.py
+++ ubuntu/.ycm_extra_conf.py
@@ -4,26 +4,28 @@ import os
 
 multiview_include_dir = os.path.join(os.environ['PERCEIVE_CODE'], 'multiview/multiview/multiview_cpp/src')
 flags = [
-	'-Wall',
-	'-Wextra',
-	'-Werror',
-	'-Wno-long-long',
-	'-Wno-variadic-macros',
-	'-fexceptions',
-	'-ferror-limit=10000',
-	'-DNDEBUG',
-	'-std=c++17',
-	'-stdlib=libc++17',
-	'-xc++',
-	'-I', '/usr/include/c++/7.1.0/',
-	'-I', multiview_include_dir,
-]
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-ferror-limit=10000',
+    '-DNDEBUG',
+    '-std=c++17',
+    '-stdlib=libc++17',
+    '-xc++',
+    '-I', '/usr/include/c++/7.1.0',
+    '-I', '/usr/include/eigen3',
+    '-I', '/usr/include/jsoncpp',
+    '-I', multiview_include_dir,
+    ]
 
 SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', ]
 
 def FlagsForFile( filename, **kwargs ):
-	return {
-		'flags': flags,
-		'do_cache': True
-	}
+    return {
+        'flags': flags,
+        'do_cache': True
+        }
 
